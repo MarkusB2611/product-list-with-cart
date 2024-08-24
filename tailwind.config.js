892de9dda43dcbbf9b3e981138318f54d539dc86
Markus Bulheller <markus.bulheller@gmail.com>
2024-08-24 14:08:47 +0200

    add design colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "plwc-red": "#C73B0F",
+        "plwc-rose": {
+          50: "#FCF8F6",
+          100: "#F5EEEC",
+          300: "#CAAFA7",
+          400: "#AD8A85",
+          500: "#87635A",
+          900: "#260F08",
+        },
+        "plwc-green": "#1EA575",
+      },
+    },
   },
   plugins: [],
 };
