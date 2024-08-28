3abf6d4192cfb1438dcb8a6f49fa0f05956fb063
Markus Bulheller <markus.bulheller@gmail.com>
2024-08-29 01:04:29 +0200

    Add add-to-cart button component
    Introduce a new `AddToCartButton` component with a button styling and an icon. This component will enhance the user experience by providing a clear call-to-action for adding items to the cart.

---
+++ src/components/add-to-cart-button/add-to-cart-button.tsx
@@ -0,0 +1,10 @@
+const AddToCartButton: React.FC = () => {
+    return (
+        <button className="w-40 h-11 text-plwc-rose-900 border-plwc-rose-400 border rounded-full hover:border-plwc-red hover:text-plwc-red flex justify-center items-center gap-2">
+            <img src="/assets/images/icon-add-to-cart.svg" alt="Add To Cart" />
+            <span>Add to Cart</span>
+        </button>
+    );
+}
+
+export default AddToCartButton;
