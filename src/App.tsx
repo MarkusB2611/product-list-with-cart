cf9b1b61f3127d8cb943b2718ca0321e07cca32a
Markus Bulheller <markus.bulheller@gmail.com>
2024-08-29 00:48:51 +0200

    Add new images, fonts, and SVG icons
    Introduced various dessert images and RedHatText fonts to the assets. Updated the index CSS with new font-face rules and applied Tailwind CSS styles to headings. Removed outdated styles from App.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import './App.css'
 
 function App() {
+
   return (
     <>
-      <h2 className="text-red-500 font-redhattext font-bold">Franz jagt im komplett verwahrlosten Taxi quer durch Bayern.</h2>
+      <div className="font-redhattext">Franz</div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
