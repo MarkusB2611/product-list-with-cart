bf2420d02d3d30b72291a348452a4b0c9cb9a155
Markus Bulheller <markus.bulheller@gmail.com>
2024-08-29 02:22:35 +0200

    Add background color to AddToCartButton
    Updated the AddToCartButton component to include a white background. This change ensures better visibility and alignment with the overall design theme.

--- src/components/add-to-cart-button/add-to-cart-button.tsx
+++ src/components/add-to-cart-button/add-to-cart-button.tsx
@@ -1,6 +1,6 @@
 const AddToCartButton: React.FC = () => {
     return (
-        <button className="w-40 h-11 text-plwc-rose-900 border-plwc-rose-400 border rounded-full hover:border-plwc-red hover:text-plwc-red flex justify-center items-center gap-2">
+        <button className="w-40 h-11 bg-white text-plwc-rose-900 border-plwc-rose-400 border rounded-full hover:border-plwc-red hover:text-plwc-red flex justify-center items-center gap-2">
             <img src="/assets/images/icon-add-to-cart.svg" alt="Add To Cart" />
             <span>Add to Cart</span>
         </button>
