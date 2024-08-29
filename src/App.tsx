bbf987cd3f83d07e1c84cd3d6723c09b98969a4b
Markus Bulheller <markus.bulheller@gmail.com>
2024-08-29 02:23:37 +0200

    Add product listing and cart components
    Introduced a JSON data import for product listings and added `ProductCard` and `Cart` components to display the items and cart details. Implemented state management for the cart using `useState` to track items, total, and count.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import './App.css'
+import DATA from '../data.json'
+import ProductCard from "./components/product-card/product-card.tsx";
+import Cart from "./components/cart/cart.tsx";
+import {useState} from "react";
+
+interface CartItems {
+    name: string,
+    price: number,
+    amount: number
+}
+
+export interface CartType {
+    items: CartItems[]
+    total: number,
+    count: number
+
+}
 
 function App() {
+    const [cart, setCart] = useState<CartType>({items: [], total: 0, count: 0});
 
   return (
-    <>
-      <div className="font-redhattext">Franz</div>
-    </>
+      <div className="my-[5.5rem] mx-28 flex gap-8">
+          <div>
+              <h1 className="text-plwc-rose-900 font-bold text-[40px] leading-[120%] mb-8">Desserts</h1>
+              <div className="grid grid-cols-3 gap-6">
+                  {DATA.map(data => {
+                      return <ProductCard key={data.name} product={data}/>
+                  })}
+              </div>
+          </div>
+          <Cart cart={cart}/>
+      </div>
   )
 }
 
